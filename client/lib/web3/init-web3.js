25330ebafee52639f4e0e69266ca05b2c79d3277
ssteiger <soren1@gmx.net>
2018-05-12 20:42:07 +0200

    clean up

--- client/lib/web3/init-web3.js
+++
@@ -1,3 +0,0 @@
-//var Web3 = require('web3');
-
-//web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
--- server/config/lib/init-web3.js
+++ server/config/lib/init-web3.js
@@ -1,3 +1,9 @@
+// https://github.com/ethereum/web3.js/
+
 var Web3 = require('web3');
 
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
